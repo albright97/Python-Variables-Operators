c96bcc1b492e5be334abfaaf89f5c41fe8fed44a
albrightbeka <31329123-albrightbeka@users.noreply.replit.com>
2024-01-17 11:03:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,111 @@
+print('Hello World!')
+#Variables
+
+age = 20
+
+print(age)
+
+#Data Types
+
+sentence = "my name is Albright"
+
+print(sentence)
+
+#Multiple Assignment
+
+sarah, bob, mike = 16, 21, 17
+
+print(sarah)
+print(bob)
+
+Albright, Judith, Judd = 16, 21, 17
+
+print(Albright, Judith, Judd)
+
+Age, Name = 26, "Albright"
+
+print(Age, Name)
+
+#Arithmetic Operators and Strings
+
+Age1 = 12
+Age2 = 18
+
+print(Age1 + Age2)
+print(Age1 - Age2)
+print(Age1 * Age2)
+print(Age1 / Age2)
+print(Age1 % Age2)
+
+sent1 = "today is a beautiful day"
+
+print(sent1)
+
+first_name = "Albright"
+last_name = "Judd"
+
+print(first_name + " " + last_name)
+
+sent = "Albright was playing basketball"
+
+print(sent[0])
+print (sent[0:8])
+
+#Placeholders in Strings
+
+name = "Albright"
+sentence = "%s %s was the president of the US"
+
+print (sentence % ("Barack", "Obama"))
+
+#format strings
+
+name = "Albright"
+print (f" hello, {name}")
+
+x = 10
+y = 20
+
+print(f"The sum of x and y is {x+y}")
+
+#Exerise
+age1 = 15
+age2 = 30
+
+print((age1 + age2) / 2)
+print(age1 + age2)
+print(age1 - age2)
+print(age1 * age2)
+print(age1 / age2)
+print(age1 % age2)
+
+name = "Albright"
+
+print(name)
+
+food1, food2, food3 = "pizza", "burger", "pasta"
+
+print(food1, food2, food3)
+
+print("Hello" * 10)
+
+name, age = "Albright", 26
+
+print(name, age)
+
+sent1 = "Hi there."
+sent2 = "How are you doing?"
+
+print(sent1 + " " + sent2)
+
+name = "Albright"
+
+print(name[3])
+
+#Introduction to Lists
+
+
+
+
+
+
